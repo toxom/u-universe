313035ac679c350fed6b0f5756807514988a3697
Janne Ruponen <ruponez@gmail.com>
2023-09-07 18:12:20 +0300

    npm firebase+mui

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhbvqar9DUQjDqbjTpztmOuMKz0OS8Bg8",
+  authDomain: "u-universe.firebaseapp.com",
+  projectId: "u-universe",
+  storageBucket: "u-universe.appspot.com",
+  messagingSenderId: "769321973541",
+  appId: "1:769321973541:web:c12af142520d7cddcecccc",
+  measurementId: "G-GVR4D9F52B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
